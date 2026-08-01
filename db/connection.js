81d88939bee19b1173e5fe34ac981d34d478d897
ElTlacuache <irving.delamora@outlook.com>
2026-07-31 21:28:11 -0600

    Refactor mesas endpoint to query available and unavailable tables simultaneously

--- db/connection.js
+++ db/connection.js
@@ -4,4 +4,5 @@ export const conexionMySQL = await mysql.createConnection({
   host: "localhost",
   user: "LeFonde",
   password: "LeContraseñe",
+  multipleStatements: true,
 });
--- routes/mesas.js
+++ routes/mesas.js
@@ -4,18 +4,16 @@ import { conexionMySQL } from "../db/connection.js";
 const router = Router();
 
 const mesas = async (req, res) => {
-  const { disponible } = req.body;
   try {
     const [mesas] = await conexionMySQL.query(
-      "SELECT * FROM lefonde.mesas WHERE disponible = ?",
-      [disponible],
+      "SELECT * FROM lefonde.mesas WHERE disponible = 1; SELECT * FROM lefonde.mesas WHERE disponible = 0",
     );
 
     if (mesas.length === 0) {
       res.send({ mensaje: "no hay datos" });
     } else {
       res.send({ mesas });
-      console.log(mesas);
+      console.log({ mesas });
     }
   } catch (error) {
     res.send(error);
