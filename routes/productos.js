5d08297109a152a3be5997c9d1fbe0fb47e5da30
Alejandro De La Mora <ht-vdis@live.com.mx>
2026-08-04 08:29:12 -0600

    Agregar endpoint de productos para manejar consultas de categorías

---
+++ routes/productos.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { conexionMySQL } from "../db/connection.js";
+
+const router = Router();
+
+const productos = async (req, res) => {
+  try {
+    const [productos] = await conexionMySQL.query(
+      "SELECT * FROM lefonde.productos WHERE categoria = 1; SELECT * FROM lefonde.productos WHERE categoria = 2; SELECT * FROM lefonde.productos WHERE categoria = 3",
+    );
+
+    if (productos.length === 0) {
+      res.send({ mensaje: "no hay datos" });
+    } else {
+      res.send({ productos });
+      console.log({ productos });
+    }
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+router.post("/productos", productos);
+
+export default router;
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import login_route from "./login.js";
 import mesas_route from "./mesas.js";
+import produ_route from "./productos.js";
 
 const router = Router();
 
 router.use(login_route);
 router.use(mesas_route);
+router.use(produ_route);
 
 export default router;
