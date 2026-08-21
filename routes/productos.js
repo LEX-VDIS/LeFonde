5d38423c9227bbfdc07a0d94d215c914c5f734ca
ElTlacuache <irving.delamora@outlook.com>
2026-08-21 15:29:19 -0600

    Agregar función getProductos para manejar solicitudes GET en la ruta /productos

--- routes/productos.js
+++ routes/productos.js
@@ -3,6 +3,23 @@ import { conexionMySQL } from "../db/connection.js";
 
 const router = Router();
 
+const getProductos = async (req, res) => {
+  try {
+    const [productos] = await conexionMySQL.query(
+      "SELECT * FROM lefonde.productos WHERE categoria = 1; SELECT * FROM lefonde.productos WHERE categoria = 2; SELECT * FROM lefonde.productos WHERE categoria = 3; SELECT * FROM lefonde.productos WHERE categoria = 4",
+    );
+
+    if (productos.length === 0) {
+      res.send({ mensaje: "no hay datos" });
+    } else {
+      res.send({ productos });
+      console.log({ productos });
+    }
+  } catch (error) {
+    res.send(error);
+  }
+};
+
 const productos = async (req, res) => {
   try {
     const [productos] = await conexionMySQL.query(
@@ -20,6 +37,7 @@ const productos = async (req, res) => {
   }
 };
 
+router.get("/productos", getProductos);
 router.post("/productos", productos);
 
 export default router;
